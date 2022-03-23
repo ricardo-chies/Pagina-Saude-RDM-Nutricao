38b5b44772052de2bf1dc149fcbb3b5b0d0d7b86
Ricardo chies <ricardochies5@gmail.com>
2022-03-23 18:10:41 -0300

    Atualização páginas

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ function calculo() {
     var peso = document.getElementById("peso").value
     var imc = parseFloat(peso) / parseFloat(altura * altura)
     var resultado = document.querySelector('span')
-    console.log(altura, peso)
-    console.log(imc)
 
     if (imc < 18.5)
-<<<<<<< HEAD
         resultado.innerHTML = "Seu IMC é: " + imc.toFixed(2) + " Classificação: Magreza"
     else if (imc < 24.9)
         resultado.innerHTML = "Seu IMC é: " + imc.toFixed(2) + " Classificação: Peso normal"
@@ -17,17 +14,5 @@ function calculo() {
         resultado.innerHTML = "Seu IMC é: " + imc.toFixed(2) + " Classificação: Obesidade"
     else
         resultado.innerHTML = "Seu IMC é: " + imc.toFixed(2) + " Classificação: Obesidade grave"
-=======
-        resultado.innerHTML = "Seu IMC é: " + imc.toFixed(2)  +  " Classificação: Magreza"
-    else if (imc < 24.9)
-        resultado.innerHTML = "Seu IMC é: " + imc.toFixed(2)  +  " Classificação: Peso normal"
-    else if (imc < 29.9)
-        resultado.innerHTML = "Seu IMC é: " + imc.toFixed(2)  +  " Classificação: Sobrepeso"
-    else if (imc < 39.9)
-        resultado.innerHTML = "Seu IMC é: " + imc.toFixed(2)  +  " Classificação: Obesidade"
-    else
-        resultado.innerHTML = "Seu IMC é: " + imc.toFixed(2)  +  " Classificação: Obesidade grave"
->>>>>>> 29009ef684298232e2a41c7b4a60b47d3b651a75
-    console.log(resultado)
 }
 
